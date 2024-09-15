233c31f00d8fb15fa7be60bd9c49d41aad5f11d0
Ayushman Rana <ayushmanr621@outlook.com>
2024-09-15 23:58:43 +0530

    responsive previous events

--- app/section/PastEvents.tsx
+++ app/section/PastEvents.tsx
@@ -4,24 +4,20 @@ import React from "react";
 
 const PastEvents = () => {
   return (
-    <section className="text-gray-300">
+    <section className="text-gray-300 py-8 md:py-16">
       <MaxWidthWrapper>
-        <h1 className="text-6xl mb-6 text-blue-400 font-bold">Recent Events</h1>
-        <div className="grid grid-cols-2">
+        <h1 className="text-4xl md:text-6xl mb-6 text-blue-400 font-bold text-center">
+          Recent Events
+        </h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
           <div>
-            <h2 className="text-4xl mb-4">Event : CodeX</h2>
-            <p className="text-lg">
+            <h2 className="text-3xl md:text-4xl mb-4">Event: CodeX</h2>
+            <p className="text-base md:text-lg">
               CodeX, organized by the ISA HIT Students Chapter, was a two-day
-              <br />
               coding competition held on September 4th and 6th. The first round,
-              <br />
               an aptitude test, assessed participants&apos; logical reasoning
-              and
-              <br />
-              problem-solving skills. The second round, held on September 6th,
-              <br />
-              was a coding challenge where competitors solved practical
-              <br />
+              and problem-solving skills. The second round, held on September
+              6th, was a coding challenge where competitors solved practical
               programming problems under time constraints.
             </p>
           </div>
