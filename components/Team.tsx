ace72a95531bc8de4b7a114b921b600b248860f4
Ayushman Rana <ayushmanr621@outlook.com>
2024-09-17 19:54:18 +0530

    reponsive team

--- components/Team.tsx
+++ components/Team.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Team = () => {
   return (
-    <div className="z-[100] sticky backdrop-blur-md h-20 inset-x-0 top-0 w-full transition-all mb-10">
-      <h1 className="text-[#c7d2fe] text-2xl md:text-4xl text-center py-4">Team of ISA & ISOI SC</h1>
+    <div className="z-[100] sticky backdrop-blur-md h-20 md:h-16 inset-x-0 top-0 w-full transition-all mb-10">
+      <h1 className="text-[#c7d2fe] text-2xl md:text-4xl text-center py-6">Team of ISA & ISOI SC</h1>
     </div>
   );
 };
