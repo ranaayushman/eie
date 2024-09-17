dedaaf8717efd70014c8c7d732be0b1593cb4c71
Ayushman Rana <ayushmanr621@outlook.com>
2024-09-17 16:26:14 +0530

    layout

--- app/section/PastEvents.tsx
+++ app/section/PastEvents.tsx
@@ -6,23 +6,25 @@ const PastEvents = () => {
   return (
     <section className="text-gray-300 py-8 md:py-16">
       <MaxWidthWrapper>
-        <h1 className="text-4xl md:text-6xl mb-6 text-[#c7d2fe] font-bold text-center">
-          Recent Events
-        </h1>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-          <div>
-            <h2 className="text-3xl md:text-4xl mb-4">Event: CodeX</h2>
-            <p className="text-base md:text-lg">
-              CodeX, organized by the ISA HIT Students Chapter, was a two-day
-              coding competition held on September 4th and 6th. The first round,
-              an aptitude test, assessed participants&apos; logical reasoning
-              and problem-solving skills. The second round, held on September
-              6th, was a coding challenge where competitors solved practical
-              programming problems under time constraints.
-            </p>
-          </div>
-          <div>
-            <Event />
+        <div className="container mx-auto px-4">
+          <h1 className="text-4xl md:text-6xl mb-6 text-[#c7d2fe] font-bold text-center">
+            Recent Events
+          </h1>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div>
+              <h2 className="text-3xl md:text-4xl mb-4">Event: CodeX</h2>
+              <p className="text-base md:text-lg">
+                CodeX, organized by the ISA HIT Students Chapter, was a two-day
+                coding competition held on September 4th and 6th. The first
+                round, an aptitude test, assessed participants&apos; logical
+                reasoning and problem-solving skills. The second round, held on
+                September 6th, was a coding challenge where competitors solved
+                practical programming problems under time constraints.
+              </p>
+            </div>
+            <div>
+              <Event />
+            </div>
           </div>
         </div>
       </MaxWidthWrapper>
