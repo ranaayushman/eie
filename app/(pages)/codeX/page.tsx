bc36eb6acd024c0343155970ef5db2ac01d95d00
Ayushman Rana <149607139+ranaayushman@users.noreply.github.com>
2024-09-25 10:02:39 +0530

    Update page.tsx

--- app/(pages)/codeX/page.tsx
+++ app/(pages)/codeX/page.tsx
@@ -13,7 +13,6 @@ const CarouselPage = () => {
 
   return (
     <div>
-      <h1 className="text-center text-3xl my-20">3D Cube Carousel</h1>
       <Carousel3D images={images} />
     </div>
   );
