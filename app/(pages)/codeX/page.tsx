f703b3a7d2f0446367db235f83267f18cc1de00a
Ayushman Rana <ayushmanr621@outlook.com>
2024-09-26 21:53:15 +0530

    mmw

--- app/(pages)/codeX/page.tsx
+++ app/(pages)/codeX/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Carousel3D from "@/components/Carousel3D";
+import MaxWidthWrapper from "@/components/mmw";
 
 const CarouselPage = () => {
   const images = [
@@ -13,20 +14,22 @@ const CarouselPage = () => {
 
   return (
     <div>
-      <h1 className="text-center text-[#c7d2fe] font-semibold text-6xl pb-20">
-        Event: CodeX
-      </h1>
-      <div className="w-full justify-center flex items-center mx-auto mb-12 text-center">
-        <p className="text-gray-200 text-xl">
-          CodeX, organized by the ISA HIT Students Chapter, was a two-day coding
-          competition held on September 4th and 6th. The first round, an
-          aptitude test, assessed participants&apos; logical reasoning and
-          problem-solving skills. The second round, held on September 6th, was a
-          coding challenge where competitors solved practical programming
-          problems under time constraints.
-        </p>
-      </div>
-      <Carousel3D images={images} />
+      <MaxWidthWrapper>
+        <h1 className="text-center text-[#c7d2fe] font-semibold text-6xl pb-20">
+          Event: CodeX
+        </h1>
+        <div className="w-full justify-center flex items-center mx-auto mb-12 text-center">
+          <p className="text-gray-200 text-xl">
+            CodeX, organized by the ISA HIT Students Chapter, was a two-day
+            coding competition held on September 4th and 6th. The first round,
+            an aptitude test, assessed participants&apos; logical reasoning and
+            problem-solving skills. The second round, held on September 6th, was
+            a coding challenge where competitors solved practical programming
+            problems under time constraints.
+          </p>
+        </div>
+        <Carousel3D images={images} />
+      </MaxWidthWrapper>
     </div>
   );
 };
